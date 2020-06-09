66ae8992e17b49a453298f6fcc33f81dddeaf9a1
pratikdutta.786@gmail.com <pratikdutta.786@gmail.com>
2020-06-10 01:26:26 +0530

    add details of movie and add new movie

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -24,3 +24,46 @@ module.exports.allList = async function(req, res){
 
     }
 }
+
+module.exports.movieDetails = async function(req, res){
+    try{
+        let movieID = await req.params.id;
+        let movie = await Movie.findOne({_id: movieID});
+        if(movie){
+            return res.status(200).json({
+                data: movie,
+                message: "success in fetching Details"
+            });
+        }else{
+            return res.status(404).json({
+                message: "Movie Not Found"
+            });
+        }
+    }catch(err){
+        console.log("AllList", err);
+        return res.status(500).json({
+            message: "Server Error"
+        });
+    }
+}
+
+module.exports.addMovie = async function(req, res){
+    try{
+        let movie = await Movie.create(req.body);
+        if(movie){
+            return res.status(200).json({
+                data: movie,
+                message: "successfully Added"
+            });
+        }else{
+            return res.status(406).json({
+                message: "Failed, Not been able to add Movie"
+            });
+        }
+    }catch(err){
+        console.log("AllList", err);
+        return res.status(500).json({
+            message: "Server Error"
+        });
+    }
+}
--- routes/api/movie.js
+++ routes/api/movie.js
@@ -5,5 +5,7 @@ const router = express.Router();
 const movieController = require("../../controllers/movie_controller");
 
 router.get("/all", movieController.allList);
+router.get("/details/:id", movieController.movieDetails);
+router.post("/addmovie", movieController.addMovie);
 
 module.exports = router;
